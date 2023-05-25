81c2a107d108e69b1d1851b30d74a965925de4b5
Farhan Asif <f.asif2010@gmail.com>
2023-05-25 22:52:51 +0600

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+//app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/public/index.html'))
+})
+
+app.get('/home', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
